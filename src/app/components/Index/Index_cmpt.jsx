4b2520a098e4355e0f0d69ae67deab40d38ba78c
shisanhuyu <shisanhuyu>
2019-04-22 12:01:13 +0800

    修改代码

--- src/app/components/Index/Index_cmpt.jsx
+++ src/app/components/Index/Index_cmpt.jsx
@@ -55,7 +55,7 @@ class Index extends React.Component {
             npassword,
             cpassword
         } = this.state;
-        if (username && password && npassword && cpassword) {
+        if (!(username && password && npassword && cpassword)) {
             message.error('参数不全');
             return;
         }
