2ff2976aa5ce7d9208cc6c34f792845bd0cfe5e3
shisanhuyu <shisanhuyu>
2019-04-22 12:03:59 +0800

    添加解析

--- svn_m.js
+++ svn_m.js
@@ -1,35 +1,36 @@
 const path = require('path');
 const fs = require('fs');
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const serve = require('koa-static');
 const views = require('koa-views');
 const Router = require('koa-router');
 const router = new Router();
-
+app.use(bodyParser());
 router.post('/changepassword', async (ctx) => {
     const body = ctx.request.body;
     const userfile = fs.readFileSync('/file/svn/svnrepos/conf/passwd').toString();
     const pattern = new RegExp(`${body.username} = ${body.password}`)
-    if(!pattern.test(userfile)){
+    if (!pattern.test(userfile)) {
         ctx.body = JSON.stringify(
             {
-                code:-1,
-                data:'账号密码不存在',
-                msg:'账号密码不存在'
+                code: -1,
+                data: '账号密码不存在',
+                msg: '账号密码不存在'
             }
         );
-    }else{
-        const nuserfile = userfile.replace(`${body.username} = ${body.password}`,`${body.username} = ${body.npassword}`)
-        fs.writeSync('/file/svn/svnrepos/conf/passwd',nuserfile);
+    } else {
+        const nuserfile = userfile.replace(`${body.username} = ${body.password}`, `${body.username} = ${body.npassword}`)
+        fs.writeSync('/file/svn/svnrepos/conf/passwd', nuserfile);
         ctx.body = JSON.stringify(
             {
-                code:0,
-                data:'修改成功',
-                msg:'修改成功'
+                code: 0,
+                data: '修改成功',
+                msg: '修改成功'
             }
         );
     }
-    
+
 })
 
 router.get('/login', async (ctx) => {
@@ -44,10 +45,10 @@ router.get('/**', async (ctx) => {
 const app = new Koa();
 
 app.use(serve(path.join(__dirname, 'public'), {
-  maxage: 365 * 24 * 60 * 60 * 1000
+    maxage: 365 * 24 * 60 * 60 * 1000
 }));
 app.use(views(__dirname + '/views', {
-  extension: 'ejs'
+    extension: 'ejs'
 }))
 app.use(router.routes()).use(router.allowedMethods())
 
